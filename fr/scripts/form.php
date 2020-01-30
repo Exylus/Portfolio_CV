b2d5313dba736ff4b2f6a49c343bb30b9028ae95
Erwan <thomy.erwan@gmail.com>
2020-01-30 21:17:08 +0100

    Changed hmtl to php. Added an email script for contact form. Started French version from top to services.

---
+++ fr/scripts/form.php
@@ -0,0 +1,20 @@
+<?php
+  $name=$_REQUEST['name'];
+  $email=$_REQUEST['email'];
+  $subject=$_REQUEST['service'];
+  $message=$_REQUEST['subject'];
+  $message= "Email: " . $email . "\r\n" . "\r\n" . $message;
+  if (($name=="")||($name=="Enter your name..")||($email=="")||($email=="Enter your email..")||($subject==""))
+    {
+      echo "All fields are required for the form to be submitted.";
+    }
+  elseif (strpos($email, '@') !== False)
+    {
+      mail("erwan@thomystery.com", $subject, $message);
+      echo "Email sent!";
+    }
+  else
+    {
+      echo "please enter a valid email adress.";
+    }
+?>
---
+++ index.php
@@ -0,0 +1,254 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="msapplication-TileColor" content="#603cba">
+	<meta name="theme-color" content="#ffffff">
+	<title>Erwan Thomy</title>
+	<link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+  <link rel="stylesheet" href="main.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  <link href="https://fonts.googleapis.com/css?family=Zhi+Mang+Xing&display=swap" rel="stylesheet">
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+	<link rel="manifest" href="/site.webmanifest">
+	<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+
+</head>
+<body>
+	<header>
+		<div class="landing-text">
+			<h1>Erwan Thomy</h1>
+			<h6>Technology Lover and Web Developer</h6>
+		</div>
+		<div class="menu-toggler">
+			<div class="bar half start"></div>
+			<div class="bar"></div>
+			<div class="bar half end"></div>
+		</div>
+		<nav class="top-nav">
+			<ul class="nav-list">
+				<li>
+					<a href="index.html" class="nav-link">Home</a>
+				</li>
+				<li>
+					<a href="#about" class="nav-link">About</a>
+				</li>
+				<li>
+					<a href="#services" class="nav-link">Services</a>
+				</li>
+				<li>
+					<a href="#portfolio" class="nav-link">Portfolio</a>
+				</li>
+				<li>
+					<a href="#experience" class="nav-link">Experience</a>
+				</li>
+				<li>
+					<a href="#contact" class="nav-link">Contact</a>
+				</li>
+			</ul>
+		</nav>
+	</header>
+		<section class="about" id="about">
+			<div class="container">
+				<div class="profile-img" data-aos="fade-right" data-aos-delay="300">
+					<img src="images/profile.jpeg" alt="">
+				</div>
+				<div class="about-details" data-aos="fade-left" data-aos-delay="600">
+					<div class="about-heading">
+					<h1>About</h1>
+					<h6>Myself</h6>
+					</div>
+					<p>
+					I am a self-taught fullstack developer, student in Graphic Design and Web Development currently located
+					in Bordeaux, France. I intend to get a Master degree in Computer programming.
+				</p>
+				</div>
+			</div>
+			<div class="social-media" data-aos="fade-in" data-aos-delay="500">
+					<ul class="nav-list">
+						<li>
+							<a href="https://www.facebook.com/erwan.thomy" class="icon-link" target="_blank">
+								<i class="fab fa-facebook-square"></i>
+							</a>
+						</li>
+						<li>
+							<a href="https://twitter.com/eXyCS" class="icon-link" target="_blank">
+								<i class="fab fa-twitter-square"></i>
+							</a>
+						</li>
+						<li>
+							<a href="https://www.linkedin.com/in/erwan-thomy" class="icon-link" target="_blank">
+								<i class="fab fa-linkedin-in"></i>
+							</a>
+						</li>
+					</ul>
+				</div>
+		</section>
+
+		<section class="services" id="services">
+			<div class="container">
+				<div class="section-heading">
+					<h1>Services</h1>
+					<h6>What I can do for you</h6>
+				</div>
+				<div class="my-skills">
+					<div class="skill" data-aos="fade-in" data-aos-delay="300">
+						<div class="icon-container">
+							<i class="fas fa-object-group" ></i>
+						</div>
+						<h1>Graphic Design</h1>
+						<p>
+						I can help you create the brand of your dreams with astonishing graphics.
+						This includes but is not limited to; Logo, banners, total rebrandings, ...
+						</p>
+					</div>
+					<div class="skill"data-aos="fade-in" data-aos-delay="600">
+						<div class="icon-container">
+							<i class="fas fa-code" ></i>
+						</div>
+						<h1>Web Development</h1>
+						<p>
+						Being a fullstack developer means, I am at ease with HTML, PHP, CSS, JS,
+						and am still on my way to mastering SQL, REACT.js and some more
+						</p>
+					</div>
+					<div class="skill" data-aos="fade-in" data-aos-delay="900">
+						<div class="icon-container">
+							<i class="fas fa-layer-group" ></i>
+						</div>
+						<h1>Web Design</h1>
+						<p>
+						Studying graphic, design, I am in constant immersion in this type of project,
+						I am able to design a fully fonctionnal and minutiously designed website.
+						</p>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="portfolio" id="portfolio">
+			<div class="container">
+				<div class="section-heading">
+					<h1>Portfolio</h1>
+					<h6>View some of my recent work</h6>
+				</div>
+			</div>
+			<div class="portfolio-item">
+				<div class="portfolio-img has-margin-right" data-aos="fade-right" data-aos-delay="300">
+					<img src="images/portitem1.jpeg" alt="">
+				</div>
+				<div class="portfolio-description" data-aos="fade-left" data-aos-delay="600">
+					<h6>Oketa</h6>
+					<h1>Japanese restaurant website rework</h1>
+					<p>
+						The website looked outdated, using [TO FILL], I decided to give it a more refreshing and user-friendly approach.
+					</p>
+					<a href="#" class="cta">Visit website</a>
+				</div>
+			</div>
+			<div class="portfolio-item">
+				<div class="portfolio-description" data-aos="fade-right" data-aos-delay="600">
+					<h6>Website 2</h6>
+					<h1>About website 2</h1>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua.
+					</p>
+					<a href="#" class="cta">Visit website</a>
+				</div>
+				<div class="portfolio-img has-margin-right" data-aos="fade-left" data-aos-delay="300">
+					<img src="images/portitem2.jpeg" alt="">
+				</div>
+			</div>
+			<div class="portfolio-item">
+				<div class="portfolio-img has-margin-right" data-aos="fade-right" data-aos-delay="300">
+					<img src="images/portitem3.jpeg" alt="">
+				</div>
+				<div class="portfolio-description" data-aos="fade-left" data-aos-delay="600">
+					<h6>Website 3</h6>
+					<h1>About website 3</h1>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua.
+					</p>
+					<a href="#" class="cta">Visit website</a>
+				</div>
+			</div>
+		</section>
+
+		<section class="experience" id="experience">
+			<div class="container">
+				<div class="section-heading">
+					<h1>Work Experience</h1>
+					<h6>Past and current Experience</h6>
+				</div>
+				<div class="timeline">
+					<ul>
+						<li class="date" data-date="2015-2017" data-aos="fade-right" data-aos-delay="200">
+							<h1>Student at Desert Mountain Highschool</h1>
+							<p>
+								Graphic Design and Computer Science
+							</p>
+						</li>
+					</ul>
+					<ul>
+						<li class="date" data-date="2017-2019" data-aos="fade-right" data-aos-delay="400">
+							<h1>Student at Saint Joseph La Salle Troyes</h1>
+							<p>
+								Science and Technology 	of Industry and Sustainable Development
+							</p>
+						</li>
+					</ul>
+					<ul>
+						<li class="date" data-date="2019-Present" data-aos="fade-right" data-aos-delay="600">
+							<h1>Student at Digital Gaming School</h1>
+							<p>
+								Graphic Design & Web Development
+							</p>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</section>
+
+		<section class="contact" id="contact">
+			<div class="container">
+				<div class="section-heading">
+					<h1>Contact</h1>
+					<h6>Let's work together.</h6>
+				</div>
+				<form action="/scripts/form.php" target="_blank" data-aos="fade-up" data-aos-delay="500">
+					<label for="name">Name:</label>
+					<input type="text" id="name" name="name" placeholder="Enter your name.." required>
+					<label for="email">Email:</label>
+					<input type="email" id="email" name="email" placeholder="Enter your email.." required>
+					<label for="service">Services:</label>
+					<select name="service" id="service">
+						<option value="Web Development">Web Development</option>
+						<option value="Other">Other</option>
+					</select>
+					<label for="subject">Subject:</label>
+					<textarea name="subject" id="subject" cols="10" rows="10"></textarea>
+					<input type="submit" value="Submit">
+				</form>
+			</div>
+		</section>
+		<footer class="copyright">
+			<div class="up" id="up">
+				<i class="fas fa-chevron-up"></i>
+			</div>
+			<p>&copy; 2019-2020 Erwan Thomy</p>
+		</footer>
+
+		<script src="https://www.google.com/recaptcha/api.js" async defer></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <script src="main.js"></script>
+    <script>
+    AOS.init();
+  </script>
+</body>
+</html>
